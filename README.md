31b7dc261c8b9b66e614559f343060b1b54d0bd2
Floris <fmeccanici@tudelft.net>
2020-03-10 21:30:47 +0100

    adding all products on one page to cart is working

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # bolScraper
+
+## Loop over links
+https://stackoverflow.com/questions/40652864/selenium-loop-page-refreshed-in-python
--- bolScraper.py
+++ bolScraper.py
@@ -2,34 +2,74 @@ import requests
 from selenium import webdriver
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.keys import Keys
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import WebDriverException
 
-driver = webdriver.Firefox()
+class bolScraper():
+    def __init__(self):
 
-driver.get("https://www.bol.com/nl/account/login.html")
-driver.get("https://www.bol.com/nl/account/login.html")
+        self.driver = webdriver.Firefox()
 
-# driver.find_element_by_link_text("Inloggen").click()
+    def login(self, email, password):
 
-driver.find_element_by_id("login_email").send_keys("florismeccanici@hotmail.com")
-driver.find_element_by_id("login_password").send_keys("fs065h0Z")
-driver.find_element_by_name("submitLogin").click()
+        self.driver.get("https://www.bol.com/nl/account/login.html")
+        self.driver.get("https://www.bol.com/nl/account/login.html")
 
-submit =  driver.find_element_by_class_name("wsp-search__btn")
-submit.click()
+        self.driver.find_element_by_id("login_email").send_keys(email)
+        self.driver.find_element_by_id("login_password").send_keys(password)
+        self.driver.find_element_by_name("submitLogin").click()
 
-add_to_cart = driver.find_element_by_id("9200000099487091")
-add_to_cart.click()
+    def clickOnMagnifyingGlass(self):
+        submit =  self.driver.find_element_by_class_name("wsp-search__btn")
+        submit.click()
 
-select = Select(driver.find_element_by_id("tst_quantity_dropdown"))
-select.select_by_value('meer')
+    def addToCartProductsOnPage(self):
+        linklist = []
+        for link in self.driver.find_elements_by_css_selector(".js_preventable_buy_action"):
+            linklist.append(link.get_attribute('href'))
 
-amount = driver.find_element_by_class_name("js_quantity_overlay_input")
-amount.send_keys("999")
+        for link in linklist:
+            self.driver.get(link)
 
-ok = driver.find_element_by_class_name("js_quantity_overlay_ok")
-ok.click()
+    def selectMaxQuantity(self):
+
+        products = self.driver.find_elements_by_id("tst_quantity_dropdown")
+        
+        for product in products:
+
+            select = Select(product)
+            select.select_by_value('meer')
+
+            amount = self.driver.find_element_by_class_name("js_quantity_overlay_input")
+            amount.send_keys("999")
+
+            ok = self.driver.find_element_by_class_name("js_quantity_overlay_ok")
+            ok.click()
+        
+    def goToBasket(self):
+        self.driver.find_element_by_id('basket').click()
+
+    def emptyBasket(self):
+        linklist = []
+        self.goToBasket()
+        for link in self.driver.find_elements_by_id('tst_remove_from_basket'):
+            linklist.append(link.get_attribute('href'))
+
+        for link in linklist:
+            self.driver.get(link)
+
+if __name__ == "__main__":
+    email = "florismeccanici@hotmail.com"
+    password = "fs065h0Z"
+
+    scraper = bolScraper()
+    scraper.login(email, password)
+
+    # scraper.emptyBasket()
+
+    # scraper.clickOnMagnifyingGlass()
+    # scraper.addToCartProductsOnPage()
+    scraper.goToBasket()
+    scraper.selectMaxQuantity()
